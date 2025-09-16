061ee8a64b8ce7e006767f734e384e7f0f286737
Nischaya Poddar <nischayapoddar8@gmail.com>
2025-09-17 00:05:02 +0530

    Add files via upload

---
+++ assignment-4/q1.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+typedef struct {
+    int front,rear;
+    int element[5];
+}queue;
+
+queue init(){
+    queue q;
+    q.front = q.rear = -1;
+    return q;
+}
+
+int isEmpty(queue q){
+    if(q.front>q.rear||q.front==-1) return 1;
+    else return 0;
+}
+
+int isFull(queue q){
+    if(q.rear == 4) return 1;
+    else return 0;
+}
+
+int front(queue q){   // to get front element
+    if(isEmpty(q)) printf("Empty");
+    else return q.element[q.front];
+} 
+
+queue enqueue(queue q, int x){  // insert element at end
+    if(isFull(q)) printf("OVERFLOW");
+    else if(isEmpty(q)){
+        q.rear = q.front = 0;  // first element inside queue
+        q.element[q.rear] = x;
+    }
+    else{
+        ++q.rear;
+        q.element[q.rear] = x;
+    }
+    return q;
+}
+
+queue dequeue(queue q){
+    if(isEmpty(q)) printf("UNDERFLOW");
+    else{
+        ++q.front;
+    }
+    return q;
+}
+
+void print(queue q){
+    if(isEmpty(q)) printf("Queue is empty");
+    else{
+        for(int i=q.front;i<=q.rear;i++){
+            printf("%d\n",q.element[i]);
+        }
+    }
+}
+
+int main(){
+    queue Q;
+    Q = init();  // queue is initialised
+    Q = enqueue(Q,3);
+    Q = enqueue(Q,2);
+    Q = enqueue(Q,1);
+    Q = enqueue(Q,6);
+    Q = enqueue(Q,7);
+    print(Q);
+    Q = dequeue(Q);
+    Q = dequeue(Q);
+    print(Q);
+    printf("front element is %d\n",front(Q));
+}
---
+++ assignment-4/q2.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#define MAX 5
+
+typedef struct{
+    int front,rear;
+    int element[MAX];
+}circularQueue;
+
+circularQueue init(){
+    circularQueue q;
+    q.front = q.rear = -1;  // empty circular queue is initialised
+    return q;
+}
+
+int isEmpty(circularQueue q){
+    if(q.front == q.rear && q.rear == -1){
+        return 1;
+    }
+    else return 0;
+}
+
+int isFull(circularQueue q){
+    if(q.front == (q.rear+1)%MAX) return 1;
+    else return 0;
+}
+
+circularQueue enqueue(circularQueue q, int x){
+    // check overflow
+    if(isFull(q)) printf("OVERFLOW");
+    else if(q.front==-1){
+        q.front = 0;
+        q.rear = (q.rear+1)%MAX;
+        q.element[q.rear] = x;
+    }
+    else{
+        q.rear = (q.rear+1)%MAX;
+        q.element[q.rear] = x;
+    }
+    return q;
+}
+
+circularQueue dequeue(circularQueue q){
+    // check for underflow
+    if(isEmpty(q)) printf("UNDERFLOW");
+    else if(q.front == q.rear){
+        // last element remains
+        q.front = q.rear = -1;  // queue is now empty
+    }
+    else{
+        q.front = (q.front + 1) % MAX;
+    }
+    return q;
+}
+
+void print(circularQueue q) {
+    if (isEmpty(q)) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Queue elements: ");
+    int i = q.front;
+    while (1) {
+        printf("%d ", q.element[i]);
+        if (i == q.rear) break;              // stop when we’ve printed last element
+        i = (i + 1) % MAX;                   // wrap-around
+    }
+    printf("\n");
+}
+
+
+int main(){
+
+    circularQueue Q;
+    Q = init();
+    Q = enqueue(Q,3);
+    Q = enqueue(Q,7);
+    Q = enqueue(Q,54);
+    Q = enqueue(Q,6);
+    Q = enqueue(Q,9);
+    print(Q);
+    Q = dequeue(Q);
+    Q = dequeue(Q);
+    print(Q);
+    return 0;
+}
